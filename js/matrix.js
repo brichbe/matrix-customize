67ad1ce6965a8e1de031f151682beefa4853f510
Ben Brichetti <brichetti.ben@idsi.com>
2017-07-13 19:00:55 -0400

    Start of adding menu button for config dialog.

--- js/matrix.js
+++ js/matrix.js
@@ -1,23 +1,31 @@
-function runAnimation(canvas, firstLetter, letterRange, randomColor, animateRate) {
+var animateIntervalId;
+
+function runAnimation(canvas, firstLetter, letterRange, randomColor,
+		animateRate) {
+	if (animateIntervalId) {
+		clearInterval(animateIntervalId);
+	}
+
 	var context = canvas.getContext("2d");
 	var screen = window.screen;
 	var width = canvas.width = screen.width;
-	var height= canvas.height = screen.height;
+	var height = canvas.height = screen.height;
 	context.clearRect(0, 0, width, height);
-	
+
 	var columns = Array(256).join(1).split('');
-	
-	context.font="30px Arial";
-	setInterval(function() {
-		context.fillStyle= "rgba(0,0,0,0.05)";
+
+	context.font = "30px Arial";
+	animateIntervalId = setInterval(function() {
+		context.fillStyle = "rgba(0,0,0,0.05)";
 		context.fillRect(0, 0, width, height);
 		context.fillStyle = "rgba(0,255,0,1)";
-		columns = columns.map(function(value, index){
+		columns = columns.map(function(value, index) {
 			if (randomColor) {
-				context.fillStyle = window.getRandomColor(); 
+				context.fillStyle = window.getRandomColor();
 			}
 			var r = Math.random();
-			context.fillText(String.fromCharCode(Math.floor(firstLetter + r * letterRange)), index * 30, value);
+			context.fillText(String.fromCharCode(Math.floor(firstLetter + r
+					* letterRange)), index * 30, value);
 			value = (value * 1 + 30);
 			return value > 768 + r * 10000 ? 0 : value;
 		});
